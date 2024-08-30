b9ab7984779b1771a315e17e63d1a16944c6d355
neutrino-ashutosh <111528467+neutrino-ashutosh@users.noreply.github.com>
2024-08-30 18:19:18 +0530

    created functionality to display added todos as list on page but cannot be retained after refreshing

--- script.js
+++ script.js
@@ -3,24 +3,34 @@ function loadTodos(){
   return todos;
 }
 
-const todoInput = document.getElementById("todoInput");
-todoInput.addEventListener("change", (event)=>{
-  const todoText = event.target.value;
-  event.target.value = todoText.trim();
-});
-
 function addTodoToLocalStorage(todoText){
   const todos = loadTodos();
   todos.todoList.push(todoText);
   localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-const submitButton = document.getElementById("addTodo");
-submitButton.addEventListener("click", (event)=>{
-  const todoText = todoInput.value;
-  if (todoText == '') {
-    alert("please enter something , it can not be empty");
-  } else {
-    addTodoToLocalStorage(todoText);
-  }
+function appendTodoInHtml(todoText){
+  const todoList = document.getElementById("todoList");
+  const todo = document.createElement("li");
+  todo.textContent = todoText;
+  todoList.appendChild(todo);
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+  const todoInput = document.getElementById("todoInput");
+  const submitButton = document.getElementById("addTodo");
+  submitButton.addEventListener("click", (event)=>{
+    const todoText = todoInput.value;
+    if (todoText == '') {
+      alert("please enter something , it can not be empty");
+    } else {
+      addTodoToLocalStorage(todoText);
+      appendTodoInHtml(todoText);
+    }
+  })
+
+  todoInput.addEventListener("change", (event)=>{
+    const todoText = event.target.value;
+    event.target.value = todoText.trim();
+  });
 })
