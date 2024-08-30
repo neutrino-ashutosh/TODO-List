4f0ed1242f82da1d77cbff670e1b269b96bac739
neutrino-ashutosh <111528467+neutrino-ashutosh@users.noreply.github.com>
2024-08-30 17:14:31 +0530

    added submit button and todoInput functionality in script.js

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 function loadTodos(){
   const todos = JSON.parse(localStorage.getItem("todos")) || {"todoList" : []};
-  
+  return todos;
 }
+
+const todoInput = document.getElementById("todoInput");
+todoInput.addEventListener("change", (event)=>{
+  const todoText = event.target.value;
+  event.target.value = todoText.trim();
+});
+
+function addTodoToLocalStorage(todoText){
+  const todos = loadTodos();
+  todos.todoList.push(todoText);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+const submitButton = document.getElementById("addTodo");
+submitButton.addEventListener("click", (event)=>{
+  const todoText = todoInput.value;
+  if (todoText == '') {
+    alert("please enter something , it can not be empty");
+  } else {
+    addTodoToLocalStorage(todoText);
+  }
+})
