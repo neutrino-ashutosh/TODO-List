5602f60fdee6d45a58f5666c167e9b0d19ed8409
neutrino-ashutosh <111528467+neutrino-ashutosh@users.noreply.github.com>
2024-09-03 18:31:27 +0530

    added more functionality to id

--- script.js
+++ script.js
@@ -3,18 +3,46 @@ function loadTodos(){
   return todos;
 }
 
-function addTodoToLocalStorage(todoText){
+function addTodoToLocalStorage(todo){
   const todos = loadTodos();
-  todos.todoList.push(todoText);
+  todos.todoList.push(todo);
   localStorage.setItem("todos", JSON.stringify(todos));
   todoInput.value = '';
 }
 
-function appendTodoInHtml(todoText){
+function appendTodoInHtml(todo){
   const todoList = document.getElementById("todoList");
-  const todo = document.createElement("li");
-  todo.textContent = todoText;
-  todoList.appendChild(todo);
+
+  const todoItem = document.createElement("li");
+
+  const textDiv = document.createElement("div");
+  
+  textDiv.textContent = todo.text;
+  todoItem.classList.add("todoItem");
+
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("todoButtons");
+
+  const editBtn = document.createElement("button");
+  editBtn.textContent = "Edit";
+  editBtn.classList.add("editBtn");
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.classList.add("deleteBtn");
+
+  const completeBtn = document.createElement("button");
+  completeBtn.textContent = "Completed";
+  completeBtn.classList.add("completeBtn");
+ 
+  wrapper.appendChild(editBtn);
+  wrapper.appendChild(deleteBtn);
+  wrapper.appendChild(completeBtn);
+
+  todoItem.appendChild(textDiv);
+  todoItem.appendChild(wrapper)
+
+  todoList.appendChild(todoItem);
 }
 
 document.addEventListener("DOMContentLoaded",()=>{
@@ -34,8 +62,8 @@ document.addEventListener("DOMContentLoaded",()=>{
     if (todoText == '') {
       alert("please enter something , it can not be empty");
     } else {
-      addTodoToLocalStorage(todoText);
-      appendTodoInHtml(todoText);
+      addTodoToLocalStorage({text: todoText, isCompleted: false});
+      appendTodoInHtml({text: todoText, isCompleted: false});
     }
   })
 
@@ -47,8 +75,6 @@ document.addEventListener("DOMContentLoaded",()=>{
 
   const todos = loadTodos();
   todos.todoList.forEach(todo => {
-    const newTodoItem = document.createElement("li");
-    newTodoItem.textContent = todo;
-    todoList.appendChild(newTodoItem);
+    appendTodoInHtml(todo);
   });
 })
